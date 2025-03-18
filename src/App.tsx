a66968bc945e4a5913424b18a8ab7a3b98d4c9cb
PiekharievaK <pechareva@gmail.com>
2025-03-18 15:15:11 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,127 @@
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import todosFromServer from './api/todos';
+import usersFromServer from './api/users';
+import todosFromServer from './api/todos';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
+import { TodoWithUser } from './types/TodoWithUser';
+import { useState } from 'react';
+import { TodoList } from './components/TodoList';
 
 export const App = () => {
+  const findOwner = (id: Todo['id']): User | undefined => {
+    return usersFromServer.find(user => user.id === id);
+  };
+
+  const todoVithUsers = todosFromServer.map(todo => {
+    const user = findOwner(todo.userId);
+
+    return { ...todo, user };
+  });
+
+  const [visibleTodos, setVisibleTodos] =
+    useState<TodoWithUser[]>(todoVithUsers);
+  const [titleValue, setTitleValue] = useState('');
+  const [userIdValue, setUserIdValue] = useState(0);
+  // const [toutchedField, setTouchedField] = useState({
+  //   titleInput: false,
+  //   userIdInput: false,
+  // });
+
+  const createTodo = (): TodoWithUser => {
+    const newId: number =
+      Math.max(...visibleTodos.map((todo: TodoWithUser) => todo.id)) + 1;
+    const user = findOwner(userIdValue);
+
+    return {
+      id: newId,
+      title: titleValue,
+      completed: false,
+      userId: userIdValue,
+      user,
+    };
+  };
+
+  const onInputChange = (value: string | number) => {
+    if (typeof value === 'string') {
+      setTitleValue(value);
+      // setTouchedField(prev => ({ ...prev, titleInput: false }));
+    } else {
+      // setTouchedField(prev => ({ ...prev, userIdInput: false }));
+      setUserIdValue(value);
+    }
+  };
+
+  const reset = () => {
+    setTitleValue('');
+    setUserIdValue(0);
+    // setTouchedField({
+    //   titleInput: false,
+    //   userIdInput: false,
+    // });
+  };
+
+  const onFormSubmit = (event: HTMLFormElement) => {
+    event.preventDefault();
+    if (titleValue.length < 1 || userIdValue === 0) {
+      return;
+    }
+
+    const newTodo = createTodo();
+
+    setVisibleTodos(prev => [...prev, newTodo]);
+    reset();
+  };
+
   return (
     <div className="App">
       <h1>Add todo form</h1>
 
-      <form action="/api/todos" method="POST">
+      <form action="/api/todos" method="POST" onSubmit={e => onFormSubmit(e)}>
         <div className="field">
-          <input type="text" data-cy="titleInput" />
-          <span className="error">Please enter a title</span>
+          <input
+            type="text"
+            name="title"
+            data-cy="titleInput"
+            placeholder="enter some text"
+            value={titleValue}
+            onChange={event => onInputChange(event.target.value)}
+            required
+            // onBlur={event =>
+            //   setTouchedField(prev => ({
+            //     ...prev,
+            //     titleInput: true,
+            //   }))
+            // }
+          />
+
+          {titleValue.length < 1 && (
+            <span className="error">Please enter a title</span>
+          )}
         </div>
 
         <div className="field">
-          <select data-cy="userSelect">
+          <select
+            data-cy="userSelect"
+            value={userIdValue}
+            onChange={event => onInputChange(+event.target.value)}
+            required
+            // onBlur={event =>
+            //   setTouchedField(prev => ({ ...prev, userIdInput: true }))
+            // }
+          >
             <option value="0" disabled>
               Choose a user
             </option>
+            {usersFromServer.map(user => (
+              <option value={user.id} key={user.id}>
+                {user.name}
+              </option>
+            ))}
           </select>
-
-          <span className="error">Please choose a user</span>
+          {userIdValue <= 0 && (
+            <span className="error">Please choose a user</span>
+          )}
         </div>
 
         <button type="submit" data-cy="submitButton">
@@ -29,33 +129,7 @@ export const App = () => {
         </button>
       </form>
 
-      <section className="TodoList">
-        <article data-id="1" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="15" className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">delectus aut autem</h2>
-
-          <a className="UserInfo" href="mailto:Sincere@april.biz">
-            Leanne Graham
-          </a>
-        </article>
-
-        <article data-id="2" className="TodoInfo">
-          <h2 className="TodoInfo__title">
-            quis ut nam facilis et officia qui
-          </h2>
-
-          <a className="UserInfo" href="mailto:Julianne.OConner@kory.org">
-            Patricia Lebsack
-          </a>
-        </article>
-      </section>
+      <TodoList todos={visibleTodos} />
     </div>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,16 @@
-export const TodoList = () => {};
+import { TodoWithUser } from '../../types/TodoWithUser';
+import { TodoInfo } from '../TodoInfo';
+
+type Props = {
+  todos: TodoWithUser[];
+};
+
+export const TodoList = ({ todos }: Props) => {
+  return (
+    <section className="TodoList">
+      {todos.map((todo: TodoWithUser) => {
+        return <TodoInfo todo={todo} key={todo.id} />;
+      })}
+    </section>
+  );
+};
